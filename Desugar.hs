939a2408d4253bde109d03855bf4abfa6bd5d9e7
brenda96011 <brendarj1124@gmail.com>
2025-11-04 18:43:12 -0600

    doc

--- Desugar.hs
+++ Desugar.hs
@@ -1,16 +1,20 @@
 -- #################################################
--- ## Archivo: Desugar.hs ##
+-- ##             Archivo: Desugar.hs             ##
 -- #################################################
+-- Este módulo implementa la fase de desazucaración del lenguaje.
+-- Convierte expresiones de la superficie (ExprS) a expresiones del core (ExprC)
+-- descartando el azúcar sintáctica como let*, cond, listas, entre otros.
+
 module Desugar (desugar, ExprC (..), Value (..), Env, Id) where
 
 
 import Parser (ExprS (..))
 
-
+-- Identificadores se representan como strings
 type Id = String
 
 -- #################################################
--- ##          DEFINICIÓN DE VALORES Y ENTORNO    ##
+-- ##        DEFINICIÓN DE VALORES Y ENTORNO      ##
 -- #################################################
 data Value
   = NumV Int
@@ -20,7 +24,7 @@ data Value
   | NilV
   deriving (Eq, Show)
 
-
+-- Ambientes de evaluación son listas de pares (nombre,valor)
 type Env = [(Id, Value)]
 
 -- #################################################
@@ -35,19 +39,19 @@ data ExprC
   | RestoreC ExprC Env -- evalúa un cuerpo con el env del closure y restaura el env del llamador
   | IfC ExprC ExprC ExprC -- Único condicional
   -- Operadores/Predicados Nucleo (Binarios o Unarios)
-  | AddC ExprC ExprC
-  | SubC ExprC ExprC
-  | MulC ExprC ExprC
-  | DivC ExprC ExprC
-  | EqC ExprC ExprC
+  | AddC ExprC ExprC -- +
+  | SubC ExprC ExprC -- -
+  | MulC ExprC ExprC -- *
+  | DivC ExprC ExprC -- /
+  | EqC ExprC ExprC -- =
   | NeqC ExprC ExprC -- !=
   | LtC ExprC ExprC -- <
   | GtC ExprC ExprC -- >
   | LteC ExprC ExprC -- <=
   | GteC ExprC ExprC -- >=
   | NotC ExprC -- not
-  | Add1C ExprC
-  | Sub1C ExprC
+  | Add1C ExprC -- +1
+  | Sub1C ExprC -- -1
   | SqrtC ExprC -- Raiz
   | ExptC ExprC ExprC -- Exponencial
   -- Primitivas de Pares y Listas
@@ -59,34 +63,43 @@ data ExprC
   deriving (Show, Eq)
 
 -- #################################################
--- ##       FUNCIONES AUXILIARES                  ##
+-- ##            FUNCIONES AUXILIARES             ##
 -- #################################################
 
 -- Transforma una lista de parámetros en lambdas anidadas (currificación)
--- Ej: (lambda (x y) body) -> (lambda (x) (lambda (y) body))
+-- Ej: (lambda (x y) body) -> LamC "x" (LamC "y" bodyC)
+-- Desazucara una función de varios argumentos en funciones unarias anidadas
+-- preservando el orden de los argumentos
 curryLambda :: [Id] -> ExprC -> ExprC
 curryLambda [] bodyC = bodyC
 curryLambda (p : ps) bodyC = LamC p (curryLambda ps bodyC)
 
 -- Transforma una aplicación múltiple en aplicaciones anidadas (asociatividad izquierda)
 -- Ej: (f a b c) -> (((f a) b) c)
+-- Si la lista de argumentos es vacía, devuelve la función sin aplicar
 applyCurried :: ExprC -> [ExprC] -> ExprC
 applyCurried funC [] = funC 
 applyCurried funC (a : args) = foldl AppC (AppC funC a) args
 
 -- Función auxiliar para desazucarizar operadores variádicos binarios
+-- Desazucara cada operando y los agrupa en operadores binarios con asociatividad izquierda
+-- Ej: (+ a b c) -> + (+ a b) c
+-- NOTA: esta función requiere al menos un operando
 desugarVariadicOp :: (ExprC -> ExprC -> ExprC) -> [ExprS] -> ExprC
 desugarVariadicOp op listExprS =
   let listExprC = map desugar listExprS
    in foldl1 op listExprC
 
 -- Función auxiliar para desazucarizar operadores variádicos de comparaciones
+-- Ej: (< a b c)  -> (if (< a b) (if (< b c) #t #f) #f)
+-- Nota: si se tienen menos de dos operandos, devuelve True por defecto
 desugarChain :: (ExprC -> ExprC -> ExprC) -> [ExprS] -> ExprC
 desugarChain comp [] = BoolC True
 desugarChain comp [_] = BoolC True
 desugarChain comp (e1: e2 : esRest) = IfC (comp (desugar e1) (desugar e2)) (desugarChain comp (e2 : esRest)) (BoolC False)
 
--- combinador Z 
+-- combinador Z (combinador de punto fijo para evaluación por valor)
+-- utilizado para desazucarar letrec
 fixCombinator :: ExprC
 fixCombinator =
   LamC "g" $
@@ -98,13 +111,20 @@ fixCombinator =
 -- #################################################
 -- ##       FUNCIÓN PRINCIPAL DE DESAZUCARIZACIÓN ##
 -- #################################################
+
+-- Función que mapea ExprS (superficie) a ExprC (core) 
 desugar :: ExprS -> ExprC
+
+-- Identificadores y valores
+-- nil es una constante tratada como la cadena vacía
 desugar (IdS "nil") = NilC
 desugar (IdS s) = IdC s
 desugar (NumS n) = NumC n
 desugar (BoolS b) = BoolC b
 
 -- Operadores Aritméticos y Relacionales (Variádicos)
+-- Operadores aritméticos binarios se anidan con asociatividad izquierda mediante desugarVariadicOp
+-- Operadores relacionales se transforman en ifs anidados con desugarChain
 desugar (AddS es) = desugarVariadicOp AddC es
 desugar (SubS es) = desugarVariadicOp SubC es
 desugar (MulS es) = desugarVariadicOp MulC es
@@ -124,6 +144,8 @@ desugar (SqrtS e) = SqrtC (desugar e)
 desugar (ExptS e1 e2) = ExptC (desugar e1) (desugar e2)
 
 -- Pares y Listas
+-- Listas se desazucaran en pares anidados que asocian a la derecha
+-- HeadS y TailC se mapean a FstC y SndC gracias a el mapeo de listas a pares
 desugar (PairS e1 e2) = PairC (desugar e1) (desugar e2)
 desugar (FstS e) = FstC (desugar e)
 desugar (SndS e) = SndC (desugar e)
@@ -133,6 +155,9 @@ desugar (HeadS e) = FstC (desugar e)
 desugar (TailS e) = SndC (desugar e)
 
 -- Condicionales
+-- If0S es azúcar sintáctica: (if (= c 0) t e) utilizando operador relacional EqC
+-- CondS con una lista de pares (guard,body) se desazucara en IfCs anidados
+-- y la cláusula final else debe estar presente, de otro modo se arroja un error 
 desugar (IfS c t e) = IfC (desugar c) (desugar t) (desugar e)
 desugar (If0S c t e) = IfC (EqC (desugar c) (NumC 0)) (desugar t) (desugar e)
 desugar (CondS clauses maybeElse) = desugarCond clauses (desugarElse maybeElse)
@@ -144,7 +169,10 @@ desugar (CondS clauses maybeElse) = desugarCond clauses (desugarElse maybeElse)
       IfC (desugar guardExpr) (desugar bodyExpr) (desugarCond rest elseExpr)
 
 -- REGLA PARA LetS (Simultáneo)
--- (let ((x v1) (y v2)) body) -> ((lambda (x y) body) v1 v2)
+-- Ej: (let ((x v1) (y v2)) body) -> ((lambda (x y) body) v1 v2)
+-- Se extraen las variables vars y se obtienen los valores desazucarados valsC,
+-- se desazucara el cuerpo y se currifica la función con curryLambda, y luego
+-- esta se le aplica a valsC con applyCurried, produciendo aplicaciones anidadas
 desugar (LetS bindings body) =
   let (vars, valsS) = unzip bindings
       valsC = map desugar valsS
@@ -153,7 +181,9 @@ desugar (LetS bindings body) =
   in applyCurried (curryLambda vars bodyC) valsC
 
 -- REGLA PARA LetStarS (Secuencial)
--- (let* ((x v1) (y v2)) body) -> ((lambda (x) ((lambda (y) body) v2)) v1)
+-- Ej: (let* ((x v1) (y v2)) body) -> ((lambda (x) ((lambda (y) body) v2)) v1)
+-- Primero se desazucara cada binding y el cuerpo, y se anidan aplicaciones de
+-- un solo argumento
 desugar (LetStarS bindings body) = desugarLetStar (map desugarBinding bindings) (desugar body)
   where
     desugarBinding (var, valS) = (var, desugar valS)
@@ -162,8 +192,11 @@ desugar (LetStarS bindings body) = desugarLetStar (map desugarBinding bindings) 
       AppC (LamC var (desugarLetStar rest bodyC)) valC
 
 -- REGLA PARA LetRecS (Recursivo)
--- (letrec ((f (lambda (p) ...))) body) ->
---   ((lambda (f) body) (Z (lambda (f) (lambda (p) ...))))
+-- Ej: (letrec ((f (lambda (p) ...))) body) ->
+--      ((lambda (f) body) (Z (lambda (f) (lambda (p) ...))))
+-- Para cada binding (f, FunS params fbody) construímos λf.(λparams.fbodyC)
+-- y (Z g), y luego anidamos las llamadas con AppC (LamC f restBodyC) z_g
+-- Si el valor en un binding no es una función, se genera un error
 desugar (LetRecS bindings body) = desugarLetRec bindings (desugar body)
   where
     desugarLetRec [] bodyC = bodyC
@@ -175,8 +208,14 @@ desugar (LetRecS bindings body) = desugarLetRec bindings (desugar body)
     desugarLetRec ((f, val) : _rest) _bodyC =
       error ("letrec: No es una función para '" ++ f ++ "' — letrec solo admite bindings de funciones en esta implementación")
 
- 
+-- REGLA PARA FunS (funciones con múltiples parámetros)
+-- Ej: (lambda (x y z) body) -> (lambda (x) (lambda (y) (lambda (z) body))) 
+-- Funciones con varios parámetros se currifican (se desazucaran usando curryLambda)
 desugar (FunS params body) = curryLambda params (desugar body)
- 
+
+-- REGLA PARA AppS (aplicaciones con múltiples argumentos)
+-- Ej: (f a b c) -> (((f a) b) c)
+-- Aplicaciones con varios argumentos se desazucaran usando applyCurried
+-- La función es aplicada a los argumentos uno por uno con asociatividad izquierda
 desugar (AppS funExpr argExprs) = applyCurried (desugar funExpr) (map desugar argExprs)
 
--- Interp.hs
+++ Interp.hs
@@ -1,6 +1,9 @@
 -- #################################################
--- ## Archivo: Interp.hs ##
+-- ##              Archivo: Interp.hs             ##
 -- #################################################
+-- Este módulo implementa el intérprete small-step del lenguaje.
+-- Define las reglas de evaluación de ExprC bajo un ambiente Env.
+
 module Interp (interp, Value(..), Env) where
 
 import Desugar (ExprC(..), Value(..), Env, Id)
@@ -11,6 +14,7 @@ import Debug.Trace (trace)
 -- ##       FUNCIONES isValue, lookup, etc.       ##
 -- #################################################
 
+-- Función que determina si una expresión en el core es un valor
 isValue :: ExprC -> Bool
 isValue (NumC _)  = True
 isValue (BoolC _) = True
@@ -18,20 +22,28 @@ isValue NilC      = True
 isValue (ValC v)  = isValueVal v
 isValue _         = False
 
+-- Función que determina si un Value está bien formado: los pares
+-- se validan recursivamente. Otros valores son válidos por construcción
 isValueVal :: Value -> Bool
 isValueVal (PairV v1 v2) = isValueVal v1 && isValueVal v2
 isValueVal _             = True
 
+-- Función que busca un identificador en el ambiente
+-- Arroja un error si el identificador no existe en el ambiente (variable libre)
 lookup :: Id -> Env -> Value
 lookup var [] = error ("Variable libre: " ++ var)
 lookup var ((name, val):rest)
   | var == name = val
   | otherwise   = lookup var rest
 
+-- Función que revisa si una expresión en el core es una abstracción lambda
+-- (no considera closures)
 isLam :: ExprC -> Bool
 isLam (LamC _ _) = True
 isLam _          = False
 
+-- Función que convierte un Value a su expresión correspondiente en el core
+-- Closures y pares se convierten en ValC v, indicando que son valores finales
 valueToExpr :: Value -> ExprC
 valueToExpr (NumV n) = NumC n
 valueToExpr (BoolV b) = BoolC b
@@ -39,6 +51,8 @@ valueToExpr NilV = NilC
 valueToExpr v@(ClosureV _ _ _) = ValC v
 valueToExpr v@(PairV _ _) = ValC v
 
+-- Función que convierte una expresión en el core, que representa un valor,
+-- en el Value que le corresponde. Arroja un error si la expresión no es un valor
 exprToValue :: ExprC -> Value
 exprToValue (NumC n) = NumV n
 exprToValue (BoolC b) = BoolV b
@@ -46,6 +60,7 @@ exprToValue NilC = NilV
 exprToValue (ValC v) = v
 exprToValue e = error ("Intentando convertir ExprC no-valor a Value: " ++ show e)
 
+-- Función que determina si una expresión es un closure ya evaluado
 isClosure :: ExprC -> Bool
 isClosure (ValC (ClosureV _ _ _)) = True
 isClosure _                       = False
@@ -53,18 +68,31 @@ isClosure _                       = False
 -- #################################################
 -- ##       FUNCIÓN PRINCIPAL: smallStep          ##
 -- #################################################
+
+-- Función que hace un paso de la reducción de una expresión 
+-- en el core bajo cierto ambiente. La evaluación es call-by-value
+-- Cada paso devuelve la nueva expresión con el ambiente actualizado
 smallStep :: ExprC -> Env -> (ExprC, Env)
 
--- Reglas para Valores y Variables 
+-- REGLAS PARA Valores y Variables
+-- Casos base: si la expresión ya es un valor, es irreducible
+-- Nota: IdC i no es un valor, se reduce a (ValC (lookup i env)),
+-- es decir, el identificador se sustituye por su valor en el ambiente
 smallStep e@(NumC _) env  = (e, env)
 smallStep e@(BoolC _) env = (e, env)
 smallStep e@NilC env      = (e, env)
 smallStep e@(ValC v) env | isValueVal v = (e, env)
 smallStep (IdC i) env = (ValC (lookup i env), env)
 
--- Regla para Funciones 
-smallStep (LamC param body) env = trace ("Creating closure for LamC. CapturedEnv: " ++ show env) $ (ValC (ClosureV param body env), env)
+-- REGLA PARA Funciones 
+-- Al evaluar una abstracción lambda, se captura el ambiente actual con un closure
+-- (alcance estático). El resultado es un valor.
+smallStep (LamC param body) env = (ValC (ClosureV param body env), env)
 
+-- REGLA PARA Condicional
+-- Primero se reduce la condición, propagando el ambiente en caso de que cambie.
+-- Si la condición es un valor, si es True la expresión se reduce a thenE,
+-- Si es False se reduce a elseE
 smallStep (IfC cond thenE elseE) env
   | isValue cond = case exprToValue cond of
                      (BoolV True)  -> (thenE, env)
@@ -72,7 +100,10 @@ smallStep (IfC cond thenE elseE) env
                      _ -> error "Condición de If no es booleana tras evaluar"
   | otherwise    = let (cond', env') = smallStep cond env in (IfC cond' thenE elseE, env') 
 
--- Reglas para Operadores Binarios y Unarios 
+-- REGLAS PARA Operadores Binarios y Unarios
+-- Los operandos se evalúan de izquierda a derecha usando smallStepBinOp y smallStepBinPred
+-- Una vez que los operandos ya son valores, se aplica la operación
+-- Los errores de tipos son señalados en tiempo de ejecución
 smallStep (AddC e1 e2) env = smallStepBinOp e1 e2 AddC (\n1 n2 -> NumC (n1 + n2)) env
 smallStep (SubC e1 e2) env = smallStepBinOp e1 e2 SubC (\n1 n2 -> NumC (n1 - n2)) env
 smallStep (MulC e1 e2) env = smallStepBinOp e1 e2 MulC (\n1 n2 -> NumC (n1 * n2)) env
@@ -100,21 +131,38 @@ smallStep (SqrtC e) env
   | isValue e = case exprToValue e of (NumV n) -> (NumC (floor (sqrt (fromIntegral n :: Double))), env); _ -> error "Sqrt requiere número"
   | otherwise = let (e', env') = smallStep e env in (SqrtC e', env') 
 
--- Reglas para Pares y Listas (sin cambios)
+-- REGLA PARA Pares y Listas
+-- Se evalúan los componentes de izquierda a derecha, cuando ya son valores, se crea un
+-- PairV y se devuelve como un valor ValC
 smallStep (PairC e1 e2) env
   | isValue e1 && isValue e2 = (ValC (PairV (exprToValue e1) (exprToValue e2)), env)
   | isValue e1               = let (e2', env2) = smallStep e2 env in (PairC e1 e2', env2) 
   | otherwise                = let (e1', env1) = smallStep e1 env in (PairC e1' e2, env1) 
 
+-- REGLA PARA proyección del primer elemento de un par
+-- Evalúa la expresión interna hasta obtener un valor (par) y devuelve
+-- el primer elemento. Arroja un error si no es un par
 smallStep (FstC e) env
   | isValue e = case exprToValue e of (PairV v1 _) -> (valueToExpr v1, env); _ -> error "Fst requiere un par"
   | otherwise = let (e', env') = smallStep e env in (FstC e', env') 
 
+-- REGLA PARA proyección del segundo elemento de un par
+-- Análogo a FstC, pero devuelve el segundo elemento
 smallStep (SndC e) env
   | isValue e = case exprToValue e of (PairV _ v2) -> (valueToExpr v2, env); _ -> error "Snd requiere un par"
   | otherwise = let (e', env') = smallStep e env in (SndC e', env') 
 
--- Regla para Aplicación 
+-- REGLA para Aplicación 
+-- 1. Si la función es un closure y el argumento ya es un valor, le aplicamos el closure:
+-- Extraemos el closure de la función a aplicar, obtenemos el Value del argumento,
+-- y se extiende el ambiente con el binding del parámetro.
+-- Se usa RestoreC para evaluar el cuerpo de la función bajo el ambiente extendido,
+-- y una vez que ya se redujo a un valor, se recupera callerEnv (ambiente en el cual
+-- se mandó a llamar la función) como ambiente del resultado.
+-- 2. De otro modo, si la función es un closure y el argumento es una lambda, se crea un closure
+-- para el argumento con el ambiente actual y seguimos como en el primer caso
+-- 3. Si la función es un closure pero el argumento aún no es un valor, se reduce el argumento
+-- 4. Si la función aún no es un closure, se reduce la función
 smallStep (AppC funExpr argExpr) env
   -- 1) Función lista y argumento ya es valor: aplicar con RestoreC
   | isClosure funExpr && isValue argExpr =
@@ -122,7 +170,7 @@ smallStep (AppC funExpr argExpr) env
         (ClosureV param body capturedEnv) ->
           let argVal = exprToValue argExpr
               newEnv = (param, argVal) : capturedEnv
-          in trace "Applying closure..." (RestoreC body env, newEnv)
+          in (RestoreC body env, newEnv)
         _ -> error "Imposible: isClosure falló (caso valor)"
   -- 2) Función lista y argumento es LamC
   | isClosure funExpr && isLam argExpr =
@@ -131,9 +179,9 @@ smallStep (AppC funExpr argExpr) env
           let (LamC argParam argBody) = argExpr
               argVal = ClosureV argParam argBody env
               newEnv = (param, argVal) : capturedEnv
-          in trace "Applying closure (arg was LamC)..." (RestoreC body env, newEnv)
+          in (RestoreC body env, newEnv)
         _ -> error "Imposible: isClosure falló (caso isLam argExpr)"
-  -- 3) La función ya es un clousure, reducir el argumento
+  -- 3) La función ya es un closure, reducir el argumento
   | isClosure funExpr =
       let (argExpr', env') = smallStep argExpr env
       in (AppC funExpr argExpr', env')
@@ -142,6 +190,10 @@ smallStep (AppC funExpr argExpr) env
       let (funExpr', env') = smallStep funExpr env
       in (AppC funExpr' argExpr, env')
 
+-- RestoreC e callerEnv indica que evaluamos e bajo otro ambiente. CallerEnv contiene
+-- el ambiente de quien hace la llamada, y es el ambiente que debe usarse para
+-- el resultado final. Si e ya es un valor se devuelve (e, callerEnv), si no,
+-- se reduce e en el ambiente actual
 smallStep (RestoreC e callerEnv) env
   | isValue e = (e, callerEnv)
   | otherwise = let (e', env') = smallStep e env in (RestoreC e' callerEnv, env')
@@ -154,6 +206,11 @@ smallStep (ValC v) env | not (isValueVal v) = error ("Error interno: smallStep e
 -- #################################################
 -- ##       FUNCIONES AUXILIARES para smallStep   ##
 -- #################################################
+
+-- Función que realiza un paso pequeño en operadores binarios aritméticos
+-- e1 y e2 son los operandos, builder es el constructor del operador, evalFunc
+-- es una función que produce la expresión resultante y env es el ambiente actual
+-- Si e1 y e2 son valores pero no son NumV, arroja un error
 smallStepBinOp :: ExprC -> ExprC -> (ExprC -> ExprC -> ExprC) -> (Int -> Int -> ExprC) -> Env -> (ExprC, Env)
 smallStepBinOp e1 e2 builder evalFunc env
   | isValue e1 && isValue e2 =
@@ -167,6 +224,9 @@ smallStepBinOp e1 e2 builder evalFunc env
       let (e1', env1) = smallStep e1 env 
       in (builder e1' e2, env1)          
 
+-- Función que realiza un paso pequeño en operadores binarios relacionales
+-- Análogo al caso de operadores binarios aritméticos
+-- evalFunc toma dos Values y produce la expresión en el core resultante (BoolC)
 smallStepBinPred :: ExprC -> ExprC -> (ExprC -> ExprC -> ExprC) -> (Value -> Value -> ExprC) -> Env -> (ExprC, Env)
 smallStepBinPred e1 e2 builder evalFunc env
   | isValue e1 && isValue e2 = (evalFunc (exprToValue e1) (exprToValue e2), env)
@@ -180,7 +240,7 @@ smallStepBinPred e1 e2 builder evalFunc env
 
 
 -- #################################################
--- ##       FUNCIÓN INTERP    ##
+-- ##                 FUNCIÓN INTERP              ##
 -- #################################################
 interp :: ExprC -> Env -> Value
 interp e env = go e env
